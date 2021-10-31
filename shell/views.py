2a0b7d4225ba34d5053406d65a5d88c450301c73
sswastik02 <sswastik02@gmail.com>
2021-10-31 16:35:56 +0530

    Implemented restricted user history for list

--- shell/views.py
+++ shell/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
 from shell.serializers import ShellSerializer
 from shell.models import Shell
 
@@ -8,6 +9,10 @@ from shell.models import Shell
 class ListShellAPIView(ListCreateAPIView):
     queryset = Shell.objects.all()
     serializer_class = ShellSerializer
+    def list(self,request,*args,**kwargs):
+        queryset = Shell.objects.filter(user=self.request.user) if request.user.is_authenticated else Shell.objects.none()
+        serializer = self.get_serializer(queryset,many=True)
+        return Response(serializer.data)
 
 class DetailShellAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Shell.objects.all()
